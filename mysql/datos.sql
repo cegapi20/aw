c142de8693df08578f7e0603af26ce6848d17423
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2019-03-07 19:06:42 +0100

    Actualizando los ficheros de datos de ejemplo

--- mysql/datos.sql
+++ mysql/datos.sql
@@ -1,63 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 26, 2016 at 06:13 PM
--- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40101 SET foreign_key_checks = 0 */;
-
---
--- Truncate table before insert `Roles`
---
-
+/*
+  Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
+*/
+TRUNCATE TABLE `RolesUsuario`;
 TRUNCATE TABLE `Roles`;
---
--- Dumping data for table `Roles`
---
+TRUNCATE TABLE `Usuarios`;
 
 INSERT INTO `Roles` (`id`, `nombre`) VALUES
 (1, 'user'),
 (2, 'admin');
 
---
--- Truncate table before insert `RolesUsuario`
---
-
-TRUNCATE TABLE `RolesUsuario`;
---
--- Dumping data for table `RolesUsuario`
---
 
 INSERT INTO `RolesUsuario` (`usuario`, `rol`) VALUES
 (1, 1),
 (2, 1),
 (2, 2);
 
---
--- Truncate table before insert `Usuarios`
---
-
-TRUNCATE TABLE `Usuarios`;
---
--- Dumping data for table `Usuarios`
---
-
+/*
+  La contraseña para ambos usuarios es '12345' 
+  pasword_hash('$2y$10$0eR.KhfTH5ybn/jlB86hwe/1nQeCKXk2RcLEjBscJbpUaF504kSOi', PASSWORD_DEFAULT) == '12345'
+*/
 INSERT INTO `Usuarios` (`id`, `username`, `password`) VALUES
 (1, 'user@example.org', '$2y$10$0eR.KhfTH5ybn/jlB86hwe/1nQeCKXk2RcLEjBscJbpUaF504kSOi'),
 (2, 'admin@example.org', '$2y$10$0eR.KhfTH5ybn/jlB86hwe/1nQeCKXk2RcLEjBscJbpUaF504kSOi');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40101 SET foreign_key_checks = 1 */;
--- mysql/estructura.sql
+++ mysql/estructura.sql
@@ -1,41 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 26, 2016 at 06:12 PM
--- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Roles`
---
-
+/*
+  Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
+*/
+DROP TABLE IF EXISTS `RolesUsuario`;
+DROP TABLE IF EXISTS `Mensajes`;
 DROP TABLE IF EXISTS `Roles`;
+DROP TABLE IF EXISTS `Usuarios`;
+
 CREATE TABLE IF NOT EXISTS `Roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(15) COLLATE utf8mb4_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `RolesUsuario`
---
-
-DROP TABLE IF EXISTS `RolesUsuario`;
 CREATE TABLE IF NOT EXISTS `RolesUsuario` (
   `usuario` int(11) NOT NULL,
   `rol` int(11) NOT NULL,
@@ -43,23 +19,13 @@ CREATE TABLE IF NOT EXISTS `RolesUsuario` (
   KEY `rol` (`rol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `Usuarios`
---
-
-DROP TABLE IF EXISTS `Usuarios`;
 CREATE TABLE IF NOT EXISTS `Usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
   `password` varchar(70) COLLATE utf8mb4_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci AUTO_INCREMENT=3 ;
-
---
--- Table structure for table `Mensajes`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 CREATE TABLE IF NOT EXISTS `Mensajes` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -69,26 +35,12 @@ CREATE TABLE IF NOT EXISTS `Mensajes` (
       PRIMARY KEY (`id`),
       KEY `usuario` (`usuario`),
       KEY `idMensajePadre` (`idMensajePadre`)
-    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4
+    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4  COLLATE=utf8mb4_spanish_ci;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `RolesUsuario`
---
 ALTER TABLE `RolesUsuario`
-  ADD CONSTRAINT `RolesUsuario_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `Usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `RolesUsuario_ibfk_2` FOREIGN KEY (`rol`) REFERENCES `Roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `RolesUsuario_usuario` FOREIGN KEY (`usuario`) REFERENCES `Usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `RolesUsuario_rol` FOREIGN KEY (`rol`) REFERENCES `Roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `Mensajes`
---
 ALTER TABLE `Mensajes`
-  ADD CONSTRAINT `Mensajes_ibfk_2` FOREIGN KEY (`idMensajePadre`) REFERENCES `Mensajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Mensajes_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `Usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  ADD CONSTRAINT `Mensajes_mensaje` FOREIGN KEY (`idMensajePadre`) REFERENCES `Mensajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Mensajes_usuario` FOREIGN KEY (`usuario`) REFERENCES `Usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
