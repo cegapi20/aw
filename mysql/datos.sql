55be628fb28a6b21db407fd264da497c140574aa
Antonio <marcospm0306@gmail.com>
2023-06-12 19:21:52 +0200

    create tables mysql

--- mysql/datos.sql
+++ mysql/datos.sql
@@ -1,13 +1,19 @@
 /*
   Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
 */
+
 TRUNCATE TABLE `RolesUsuario`;
 TRUNCATE TABLE `Roles`;
 TRUNCATE TABLE `Usuarios`;
+TRUNCATE TABLE `Tratamientos`;
 
 INSERT INTO `Roles` (`id`, `nombre`) VALUES
 (1, 'admin'),
 (2, 'user');
+
+INSERT INTO `Tratamientos` (`id`, `nombre`) VALUES
+(1, 'Profesor'),
+(2, 'profesora');
 
 
 INSERT INTO `RolesUsuario` (`usuario`, `rol`) VALUES
@@ -19,7 +25,8 @@ INSERT INTO `RolesUsuario` (`usuario`, `rol`) VALUES
   user: userpass
   admin: adminpass
 */
-INSERT INTO `Usuarios` (`id`, `nombreUsuario`, `nombre`, `password`) VALUES
-(1, 'admin', 'Administrador', '$2y$10$O3c1kBFa2yDK5F47IUqusOJmIANjHP6EiPyke5dD18ldJEow.e0eS'),
-(2, 'user', 'Usuario', '$2y$10$uM6NtF.f6e.1Ffu2rMWYV.j.X8lhWq9l8PwJcs9/ioVKTGqink6DG');
+INSERT INTO `Usuarios` (`id`, `nombreUsuario`, `nombre`, `password`,`tratamiento_id`) VALUES
+(1, 'admin', 'Administrador', '$2y$10$O3c1kBFa2yDK5F47IUqusOJmIANjHP6EiPyke5dD18ldJEow.e0eS',2),
+(2, 'user', 'Usuario', '$2y$10$uM6NtF.f6e.1Ffu2rMWYV.j.X8lhWq9l8PwJcs9/ioVKTGqink6DG',1),
+(3, 'antonio', 'antoniogp', '$2y$10$uM6NtF.f6e.1Ffu2rMWYV.j.X8lhWq9l8PwJcs9/ioVKTGqink6DG',1);
 
--- mysql/estructura.sql
+++ mysql/estructura.sql
@@ -3,7 +3,12 @@
 */
 DROP TABLE IF EXISTS `RolesUsuario`;
 DROP TABLE IF EXISTS `Roles`;
+DROP TABLE IF EXISTS `ListasAsistenciaEstudiantes`;
+DROP TABLE IF EXISTS `EstudiantesClase`;
+DROP TABLE IF EXISTS `ListasAsistencia`;
+DROP TABLE IF EXISTS `Clases`;
 DROP TABLE IF EXISTS `Usuarios`;
+DROP TABLE IF EXISTS `Tratamientos`;
 
 CREATE TABLE IF NOT EXISTS `Roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -18,10 +23,50 @@ CREATE TABLE IF NOT EXISTS `RolesUsuario` (
   KEY `rol` (`rol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE IF NOT EXISTS `Tratamientos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `Usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombreUsuario` varchar(30) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
   `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
   `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `tratamiento_id` int,
+   `user_photo` varchar(255) NOT NULL,
+   FOREIGN KEY (`tratamiento_id`) REFERENCES `Tratamientos`(`id`),
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Clases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `acronimo` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `acronimoTitulacion` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `profesor_id` int  NOT NULL,
+  FOREIGN KEY (`profesor_id`) REFERENCES `Usuarios`(`id`),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `EstudiantesClase` (
+  `clase_id` int NOT NULL,
+   `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
+  FOREIGN KEY (`clase_id`) REFERENCES `Clases`(`id`),
+  PRIMARY KEY (`clase_id`,`nombre`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `ListasAsistencia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `clase_id` int  NOT NULL,
+  `fecha` DATE not null,
+  FOREIGN KEY (`clase_id`) REFERENCES `Clases`(`id`),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `ListasAsistenciaEstudiantes` (
+  `lista_asistencia_id` int  NOT NULL,
+  `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  FOREIGN KEY (`lista_asistencia_id`) REFERENCES `ListasAsistencia`(`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
